51f562258828bc17e45c2089dd84617b0d3c622f
Jeff Schnitzer <jeff@infohazard.org>
2021-03-26 23:51:40 -0700

    Add --merch and --secret options

--- downloader.py
+++ downloader.py
@@ -4,12 +4,17 @@
 #
 # Usage: python downloader.py /path/to/mydesigns.csv /destination/path
 #
+# If you want OrbitKit to force the designs into the 4500x5400 merch format,
+# pass the --merch and --secret flags. You will need to get the secret from
+# your organization's settings page. Example
+#
+# python downloader.py --merch --secret 123456890 /path/to/mydesigns.csv /destination/path
+#
 
+import argparse
 import csv
-import sys
 import os
 import urllib.request
-import argparse
 
 
 parser = argparse.ArgumentParser(
@@ -17,6 +22,8 @@ parser = argparse.ArgumentParser(
     description="Download design image files from OrbitKit."
 )
 parser.add_argument("-u", "--unique", action='store_true', help='Make all filenames unique by prepending the design ID')
+parser.add_argument("-m", "--merch", action='store_true', help='Intelligently convert images to merch format, 4500x5400')
+parser.add_argument("-s", "--secret", help='Specify your organization secret key, required for --merch')
 parser.add_argument('sourcecsv', help='Metadata CSV file you downloaded from OrbitKit')
 parser.add_argument('destdir', help='Destination directory for image files')
 
@@ -25,6 +32,20 @@ args = parser.parse_args()
 csv_path = args.sourcecsv
 dest_path = args.destdir
 unique = args.unique
+merch = args.merch
+secret = args.secret
+
+if merch and not secret:
+    raise Exception('When specifying --merch, you must also specify --secret')
+
+
+# For some crazy reason cloudflare blocks the python user agent
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "OrbitKit Downloader"
+
+
+opener = AppURLopener()
+
 
 with open(csv_path, newline='', encoding='utf-8') as csv_file:
     row_count = sum(1 for row in csv.reader(csv_file)) - 1  # skip the header row
@@ -36,11 +57,12 @@ with open(csv_path, newline='', encoding='utf-8') as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
         count = count + 1
-        url = row['ART']
+        art = row['ART']
         filename = row['FILENAME']
+        key = row['ID']
 
         if unique:
-            filename = row['ID'] + '-' + filename
+            filename = key + '-' + filename
 
         print(f'Downloading {count}/{row_count} {filename}')
 
@@ -48,5 +70,8 @@ with open(csv_path, newline='', encoding='utf-8') as csv_file:
 
         if not os.path.exists(path):
             download_path = path + '.downloading'
-            urllib.request.urlretrieve(url, download_path)
+
+            url = f'https://app.orbitkit.com/api/designs/{key}/render?X-Secret={secret}' if merch else art
+
+            opener.retrieve(url, download_path)
             os.rename(download_path, path)
